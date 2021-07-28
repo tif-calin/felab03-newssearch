7667140e9c7f043742d693fd0dc31ea03ffce1eb
culi <culitif@tuta.io>
2021-07-28 16:21:32 -0700

    setup and readme

---
+++ REAME.md
@@ -0,0 +1,12 @@
+# lab03: **news search**
+## project goals
+- [ ] search for news
+- [ ] display list of news items
+- [ ] snapshot test for news item display
+- [ ] behavior test for news search
+- [ ] mock the api in a test
+
+## useful links
+ - [canvas assignment](https://canvas.instructure.com/courses/3106948/assignments/23310046?module_item_id=49574534)
+ - [github rubric and demo](https://github.com/alchemycodelab/alchemy-fsjs-june-2021/tree/main/05_react/03_forms)
+ - [news api](https://newsapi.org/)
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+import { NewsSearch } from '../../containers/NewsSearch';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <main className="App">
+    <Router>
+      <Switch>
+
+        <Route exact path="/" render={() => (
+          <Redirect to="/news"/>
+        )}/>
+
+        <Route path="/news" render={routerProps => 
+          <NewsSearch {...routerProps}/>
+        }/>
+
+        <Route path="/news/:id" render={routerProps => 
+          <NewsSearch {...routerProps}/>
+        }/>
+
+        <Redirect to="/"/>
+
+      </Switch>
+    </Router>
+  </main>;
 }
